63c8c484f24c83a737180c5864407ce0ac2c3ab0
Alexey Shishkov <cookies.eater.03@gmail.com>
2018-05-21 15:28:11 +0300

    Dynamic version

--- example/src/components/AppFooter.vue
+++ example/src/components/AppFooter.vue
@@ -2,7 +2,19 @@
   <a-lte-footer>
     <strong>Copyright © 2018.</strong> All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <strong>Version:</strong> 0.1.0
+      <strong>Version:</strong> {{ version }}
     </div>
   </a-lte-footer>
 </template>
+
+<script>
+import npmPackage from '@/../../package.json';
+
+export default {
+  data() {
+    return {
+      version: npmPackage.version,
+    };
+  },
+};
+</script>
